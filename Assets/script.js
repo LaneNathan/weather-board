2626813c11aa1d02bea026b8f66424a54253165e
Nathan Lane <lanenathan214@gmail.com>
2024-05-03 11:44:39 -0400

    wote function to display search history

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,38 @@ var searched= [];
 var weatherApiUrl = "https://api.openweathermap.org";
 var weatherApiKey ='e8591d7b37c53d2741fdfd5e36865874';
 
-var searchForm = document.querySelector();
-var searchInput = document.querySelector();
-var todayDisplay = document.querySelector();
-var weekDisplay = document.querySelector();
-var searchHistory = document.querySelector();
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#form-control');
+var todayDisplay = document.querySelector('#searched-display-today');
+var weekDisplay = document.querySelector('#week-display');
+var searchHistory = document.querySelector('#searched-history-list');
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function renderSearchHistory(){
+    searchHistory.innerHTML = '';
+
+    for(var i = searched.length - 1; i >= 0; i--){
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+
+        btn.setAttribute('data-search', searched[i]);
+        btn.textContent = searched[i];
+        searchHistory.append(btn);
+    }
+}
+
+
+function displayHistory(searches){
+    if (searched.indexOf(searches) !== -1){
+        return;
+    }
+    searched.push(searches);
+
+    localStorage.setItem('search-history', JSON.stringify(searched));
+    renderSearchHistory();
+}
+
