dd37554e228c201d236c294d60e7d0066a3255b7
Nathan Lane <lanenathan214@gmail.com>
2024-05-02 23:59:12 -0400

    set variables

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var searched= [];
+var weatherApiUrl = "https://api.openweathermap.org";
+var weatherApiKey ='e8591d7b37c53d2741fdfd5e36865874';
+
+var searchForm = document.querySelector();
+var searchInput = document.querySelector();
+var todayDisplay = document.querySelector();
+var weekDisplay = document.querySelector();
+var searchHistory = document.querySelector();
